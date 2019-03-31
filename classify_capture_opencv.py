07d1d2bd9dea744a786454f60066b08b490b38ed
Kurt Mueller <kurt.mueller@thelevelup.com>
2019-03-30 20:51:19 -0400

    OK MERGE

--- classify_capture_opencv.py
+++ classify_capture_opencv.py
@@ -52,33 +52,34 @@ def main():
         while True:
             ret, frame = cap.read()
 
-            frame = cv2.resize(frame, (width, height))
+            # the resized version of the frame will be used for analysis
+            resized = cv2.resize(frame, (width, height))
 
             if args.synchronous:
                 child_result = []
-                analyzed_frame(frame, child_result, engine)
+                analyze_frame(resized, child_result, engine, labels)
             else:
                 if not is_processing:
                     # kick off analysis in subprocess, if not currently analyzing
                     is_processing = True
-                    child_result = []
-                    child = multiprocessing.Process(target=analyze_frame, args=(frame, child_result))
+                    next_child_result = []
+                    child = multiprocessing.Process(target=analyze_frame, args=(resized, child_result, engine, labels))
                     child.start()
                 elif child is not None and not child.is_alive():
                     child.join()
-                    # child has finished processing, grab results
-                    # TODO:figure out what to do with the results, they'll need to be added to the raw frame we send to stdout
-                    pass
+                    child_result = next_child_result
+
+            frame = draw_boxes_on_frame(child_result, frame)
 
             if args.local:
+                # weird black magic needed to draw to the screen
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
                 cv2.imshow('frame', frame)
             else:
-                sys.stdout.buffer.write(frame.tostring())
+                sys.stdout.buffer.write(frame.tobytes())
 
 
-    except Exception as e:
-        print('thats no good!')
-        print(e)
     except KeyboardInterrupt:
         print('Shutting down...')
     finally:
@@ -89,7 +90,7 @@ def main():
 #output is (boxes, frame) where
 #boxes is a list of bounding boxes added
 #frame is the stillframe with the boxes added
-def analyze_frame(frame, boxes, engine):
+def analyze_frame(frame, boxes, engine, labels):
     img = Image.fromarray(frame)
     ans = engine.DetectWithImage(img, threshold=0.05, keep_aspect_ratio=True,
         relative_coord=False, top_k=10)
@@ -118,9 +119,14 @@ def draw_boxes_on_picture(boxes, img):
         absoluteBox = (int(box[0] * width), int(box[1] * height), int(box[2] * width), int(box[3] * height)) 
         draw.rectangle(absoluteBox, outline='red')
 
+def draw_boxes_on_frame(boxes, frame):
+    img = Image.fromarray(frame)
+    draw_boxes_on_picture(boxes, img)
+    return np.array(img)
+
 
 def label_is_cat(label_id):
-    (label_id == 16) or (label_id == 17) or (label_id == 73) or (label_id == 87)
+    return (label_id == 16) or (label_id == 17) or (label_id == 73) or (label_id == 87)
 
 if __name__ == '__main__':
     main()
