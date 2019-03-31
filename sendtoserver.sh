06d4af6cd66a255dd749b3648c67f1d7a6629d41
Lukas <lukas.hillmer@gmail.com>
2019-03-31 12:29:03 -0400

    added sendtoserver

---
+++ sendtoserver.sh
@@ -0,0 +1,1 @@
+python3 classify_capture_opencv.py --label ~/Downloads/canned_models/coco_labels.txt --model ~/Downloads/canned_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite --synchronous | ffmpeg -f rawvideo -r 15 -s 640x480 -pix_fmt bgr24 -i - -c:v libx264 -preset fast -pix_fmt yuv420p -s 640x480 -r 15 -g 30 -f flv rtmp://localhost/live/cat
