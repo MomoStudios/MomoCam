de24a686670fe4e9bcdfa606e9ca234334fd76e7
Lukas <lukas.hillmer@gmail.com>
2019-03-30 21:15:04 -0400

    working commit

--- classify_capture_opencv.py
+++ classify_capture_opencv.py
@@ -43,7 +43,7 @@ def main():
     engine = DetectionEngine(args.model)
 
     try:
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(2)
         _, width, height, channels = engine.get_input_tensor_shape()
         font = cv2.FONT_HERSHEY_SIMPLEX
         is_processing = True
@@ -52,33 +52,34 @@ def main():
         while True:
             ret, frame = cap.read()
 
-            frame = cv2.resize(frame, (width, height))
+            # the resized version of the frame will be used for analysis
+            resized = cv2.resize(frame, (width, height))
 
             if args.synchronous:
                 child_result = []
-                analyzed_frame(frame, child_result, engine)
+                analyze_frame(resized, child_result, engine, labels, args.local)
             else:
                 if not is_processing:
                     # kick off analysis in subprocess, if not currently analyzing
                     is_processing = True
-                    child_result = []
-                    child = multiprocessing.Process(target=analyze_frame, args=(frame, child_result))
+                    next_child_result = []
+                    child = multiprocessing.Process(target=analyze_frame, args=(resized, child_result, engine, labels, args.local))
                     child.start()
                 elif child is not None and not child.is_alive():
                     child.join()
-                    # child has finished processing, grab results
-                    # TODO:figure out what to do with the results, they'll need to be added to the raw frame we send to stdout
-                    pass
+                    child_result = next_child_result
+
+            frame = draw_boxes_on_frame(child_result, frame)
 
             if args.local:
+                # weird black magic needed to draw to the screen
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
                 cv2.imshow('frame', frame)
             else:
-                sys.stdout.buffer.write(frame.tostring())
+                sys.stdout.buffer.write(frame.tobytes())
 
 
-    except Exception as e:
-        print('thats no good!')
-        print(e)
     except KeyboardInterrupt:
         print('Shutting down...')
     finally:
@@ -89,32 +90,41 @@ def main():
 #output is (boxes, frame) where
 #boxes is a list of bounding boxes added
 #frame is the stillframe with the boxes added
-def analyze_frame(frame, boxes, engine):
+def analyze_frame(frame, boxes, engine, labels, local):
     img = Image.fromarray(frame)
     ans = engine.DetectWithImage(img, threshold=0.05, keep_aspect_ratio=True,
         relative_coord=False, top_k=10)
+    _, width, height, _ = engine.get_input_tensor_shape()
     for obj in ans:
-        print ('-----------------------------------------')
-        if labels:
-            print(labels[obj.label_id])
-        print ('score = ', obj.score)
+        if local:
+            print ('-----------------------------------------')
+            if labels:
+                print(labels[obj.label_id])
+            print ('score = ', obj.score)
         box = obj.bounding_box.flatten().tolist()
-        print ('box = ', box)
+        relative_box = (box[0] / width, box[1] / height, box[2] / width, box[3] / height)
         # Draw a rectangle.
         if label_is_cat(obj.label_id):
-            boxes.append(box)
+            boxes.append(relative_box)
     # draw_boxes_on_picture(boxes, img)
     # out = np.array(img)
 
 #draws the specified boxes in red on the input picture
 def draw_boxes_on_picture(boxes, img):
+    width, height = img.size
     draw = ImageDraw.Draw(img)
     for box in boxes:
-        draw.rectangle(box, outline='red')
+        absoluteBox = (int(box[0] * width), int(box[1] * height), int(box[2] * width), int(box[3] * height)) 
+        draw.rectangle(absoluteBox, outline='red')
+
+def draw_boxes_on_frame(boxes, frame):
+    img = Image.fromarray(frame)
+    draw_boxes_on_picture(boxes, img)
+    return np.array(img)
 
 
 def label_is_cat(label_id):
-    (label_id == 16) or (label_id == 17) or (label_id == 73) or (label_id == 87)
+    return (label_id == 16) or (label_id == 17) or (label_id == 73) or (label_id == 87)
 
 if __name__ == '__main__':
     main()
--- rundemo.sh
+++ rundemo.sh
@@ -1,1 +1,1 @@
-python3 classify_capture_opencv.py --label ~/Downloads/canned_models/imagenet_labels.txt --model ~/Downloads/canned_models/mobilenet_v2_1.0_224_quant_edgetpu.tflite --synchronous --local
+python3 classify_capture_opencv.py --label ~/Downloads/canned_models/coco_labels.txt --model ~/Downloads/canned_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite --synchronous | ffmpeg -f rawvideo -r 15 -s 640x480 -pix_fmt bgr24 -i - -c:v libx264 -preset fast -pix_fmt yuv420p -s 640x480 -r 15 -g 30 -f flv rtmp://live-jfk.twitch.tv/app/$twitch
